b2634a6d14d36f73519b18ac084bfe8565b43e7b
Artem <mehedenko888@gmail.com>
2023-02-12 22:14:23 +0200

    initial commit

--- mein.py
+++ mein.py
@@ -1,1 +1,17 @@
-print("Бажаю вам найкращого дня, чудового вечора, мирного неба над головою!!")
+from tkinter import *  # Імпортування головної бібліотеки Tkinter
+
+mein = Tk()  # Створення основний клас застосунку Tkinter.
+mein.config(bg='RED', relief=RIDGE, bd=15, height=500, width=500)  #Відповідає за вигляд вікна, RIDGE - вигляді хребта
+# bg колір фону
+# bd ширина рамки
+
+mein.title('Добрий день!')  # Назва вікна
+title = Label(text='Текст', bg='RED', font=10)  # Робить текст
+title.pack() # Створює текст
+bth = Button(text='Kопка', bg='Green') # Робить кнопку
+bth.pack() # Створює кнопку
+
+mein.mainloop()  # Запуск вікна на виконання
+
+
+
